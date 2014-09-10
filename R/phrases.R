50636fb8114febf88df09f1fa0696c812c85f31a
Flic Anderson <FlicAnderson@users.noreply.github.com>
2014-09-10 18:21:12 +0100

    Update phrases.R to English UK
    (...and now, with punctuation!)

--- R/phrases.R
+++ R/phrases.R
@@ -3,29 +3,29 @@ praise <- function() {
   swirl_is_fun <- getOption("swirl_is_fun")
   
   if(is.null(swirl_is_fun) || isTRUE(swirl_is_fun)) {
-    phrases <- c("Not bad",
-                 "What you've done is pretty good so far",
-                 "Quite nice",
-                 "That's probably how I would have done that too",
-                 "You're improving",
-                 "Not as bad as it could have been, certainly",
-                 "Good stuff",
-                 "Better than some",
+    phrases <- c("Not bad.",
+                 "What you've done is pretty good so far.",
+                 "Quite nice.",
+                 "That's probably how I would have done that too.",
+                 "You're improving.",
+                 "Not as bad as it could have been, certainly.",
+                 "Good stuff!",
+                 "Better than some.",
                  "Gosh, would you look at that...",
-                 "Adequate",
-                 "Yarp",
-                 "Well played",
+                 "Adequate.",
+                 "Yarp!",
+                 "Well played.",
                  "Got there in the end, eh?",
-                 "You seem to have gotten it right",
-                 "That is correct",
+                 "You seem to have gotten it right.",
+                 "That is correct.",
                  "Perhaps you've done this before?",
-                 "Rather good",
-                 "This will suffice",
-                 "Very nice",
-                 "I admire your work",
-                 "That seems to be the answer I was looking for")
+                 "Rather good.",
+                 "This will suffice.",
+                 "Very nice.",
+                 "I admire your work.",
+                 "That seems to be the answer I was looking for.")
   } else {
-    phrases <- "Indeed"
+    phrases <- "Indeed!"
   }
   sample(phrases, 1)
 }
@@ -35,22 +35,22 @@ tryAgain <- function() {
   swirl_is_fun <- getOption("swirl_is_fun")
   
   if(is.null(swirl_is_fun) || isTRUE(swirl_is_fun)) {
-    phrases <- c("Worse things happen at sea, old chap",
+    phrases <- c("Worse things happen at sea, old chap... Once again?",
                  "Oh dear...  :s",
-                 "Sadly not",
+                 "Sadly not, but there's always next time?",
                  "Perhaps you meant something else?",
                  "Maybe have a nice cuppa and try again?",
-                 "Almost",
-                 "Terribly sorry, but that's not correct I'm afraid",
+                 "Almost. Fancy another go at it?",
+                 "Terribly sorry, but that's not correct I'm afraid. Again?",
                  "Maybe next time, eh?",
                  "Not quite up to standard - have another go!",
                  "As my aunt used to say: 'Nope'",
                  "Come on, Tim!",
-                 "Try again",
-                 "Could be better",
-                 "Narp")
+                 "Try again!",
+                 "Could be better, how about another attempt?",
+                 "Narp!")
   } else {
-    phrases <- "Not really, try again"
+    phrases <- "Not really, try again!"
   }
   sample(phrases, 1)
 }
